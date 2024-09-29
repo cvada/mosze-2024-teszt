8cf6c171514f8df337ef9b83b6c34f54c0fea20f
cvada <gfinfgin@gmail.com>
2024-09-29 17:06:36 +0200

    hibak kijavitva

--- main.cpp
+++ main.cpp
@@ -4,28 +4,30 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];  // rossz: N_ELEMENTS nev helyett NELEMENTS ami nem letezik
-    std::cout << '1-100 ertekek duplazasa';
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
-    for (int i = 0;)  // rossz: a for ciklus hianyos
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
 
-    for (int i = 0; i; i++)  // rossz: a ciklus feltétele mindig hamis lesz, mert i kezdetben 0, így a ciklus nem fut le
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:";
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
 
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
 
-    for (int i = 0; i < N_ELEMENTS, i++)  // rossz: vesszo, helyette pontosvesszo
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0; // memoriaszivargas
+
+    delete[] b;
+    return 0;
 }
